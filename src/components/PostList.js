ad6c08364db6d2a8694607463fd03ccea4b0514a
unknown <renan.coding@gmail.com>
2020-07-11 15:52:48 -0300

    Implementa botões

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,14 +3,15 @@ import React from 'react';
 import PostPreview from './PostPreview'
 
 class PostList extends React.Component {
+    hotURL = 'https://www.reddit.com/r/reactjs/hot.json';
+    newURL = 'https://www.reddit.com/r/reactjs/new.json';
+    risingURL = 'https://www.reddit.com/r/reactjs/rising.json';
+
     constructor(props) {
         super(props);
-
-        const url = 'https://www.reddit.com/r/reactjs/hot.json';
-
         this.state = {loaded: false};
 
-        fetch(url)
+        fetch(this.hotURL)
             .then(res=>{
                 res.json()
                     .then(data=>{
@@ -20,12 +21,57 @@ class PostList extends React.Component {
             })
     }
 
+    handleClickNew = () => {
+        this.setState({loaded: false});
+        fetch(this.newURL)
+            .then(res=>{
+                res.json()
+                    .then(data=>{
+                        this.setState({loaded: true, data: data});
+                    })
+            })
+    }
+
+    handleClickHot = () => {
+        this.setState({loaded: false});
+        fetch(this.hotURL)
+            .then(res=>{
+                res.json()
+                    .then(data=>{
+                        this.setState({loaded: true, data: data});
+                    })
+            })
+    }
+
+    handleClickRising = () => {
+        this.setState({loaded: false});
+        fetch(this.risingURL)
+            .then(res=>{
+                res.json()
+                    .then(data=>{
+                        this.setState({loaded: true, data: data});
+                    })
+            })
+    }
+
     render() {
     if(this.state.loaded){
+        const hotButton= 
+            <button onClick={this.handleClickHot}>
+                hot
+            </button>;
+        const newButton=
+            <button onClick={this.handleClickNew}>
+                New
+            </button>;
+        const risingButton=
+            <button onClick={this.handleClickRising}>
+                Rising
+            </button>;
         var postList = this.state.data.data.children.map((post) => 
             <PostPreview {...post} />
         );
-        return postList;
+        return [hotButton, newButton, risingButton, postList];
     }else{
         return (<p>loading</p>);
     }
