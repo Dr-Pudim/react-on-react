c5616890dc910eb1751244464949910f260d23eb
Dr-Pudim <renan.coding@gmail.com>
2020-07-23 12:36:08 -0300

    Estiliza componente de tela de erro

--- src/components/ErrorScreen.js
+++ src/components/ErrorScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import sadFace from '../sad-face-in-rounded-square.svg';
 
 function ErrorScreen(props){
     return(
         <div className="ErrorScreen">
             <h2>{props.message}</h2>
+            <img src={sadFace} alt="sad face" title="Icon made by Freepik at https://www.flaticon.com/"/>
         </div>
     );
 }
